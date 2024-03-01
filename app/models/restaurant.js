59edddc1d952e53c090b6ce905563578f34d4740
Harish A <78593477+harish722001@users.noreply.github.com>
2024-03-01 22:40:17 +0530

    Create restaurant.js

---
+++ app/models/restaurant.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose')
+
+const restaurantSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    user_id: {
+        type: String,
+        required: true
+    },
+    phone_number: {
+        type: String,
+        required: true
+    },
+    alternate_number: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    dishes: {
+        type: [
+            {
+                name: {
+                    type: String,
+                    required: true
+                },
+                description: {
+                    type: String
+                },
+                price: {
+                    type: Number,
+                    required: true
+                },
+                serves: {
+                    type: String
+                },
+                last_modified_on: {
+                    type: Date
+                },
+                rating: {
+                    type: Number
+                }
+            }
+        ]
+    },
+    rating: {
+        type: Number
+    },
+    last_modified_on: {
+        type: Date
+    },
+    created_on: {
+        type: Date
+    }
+})
+
+module.exports = mongoose.model('restaurant', restaurantSchema)
